4e3a498497b5f1f4107be7f4f064f8b8c8e6dd1b
Jeff R. Allen <jeff.allen@epfl.ch>
2018-04-26 16:56:48 +0200

    Improve HTML.

--- src/components/html-iframe/html-iframe.js
+++ src/components/html-iframe/html-iframe.js
@@ -74,9 +74,9 @@ export default class HTMLIFrame extends React.Component {
     // EVOTING HACK: We are stealing this iframe to do our own user education.
     return (
 	<div style={{padding: '20px'}}>
-	<p>Félicitations, vous avez trouvé la page "Comment ça marche". Si vous voulez juste voter, cliquez sur "retour" dans votre navigateur.</p>
+	<p>Félicitations, vous avez trouvé la page "Comment ça marche". Si vous voulez seulement voter, cliquez sur "retour" dans votre navigateur.</p>
 	<p>Congratulations, you&#39;ve found the "How this Works" page. If you just want to vote, click "back" in your browser.</p>
-	<p>Otherwise, take a moment to <a href="https://github.com/dedis/epfl-evoting/tree/master/evoting/about">read an explanation</a> of what you see here.
+	<p>Otherwise, take a moment to <a href="https://github.com/dedis/epfl-evoting/tree/master/evoting/about/README.md">read an explanation</a> of what you see here.
 	This page works best from the EPFL network, so if you are outside you may want to turn on your VPN now.</p>
 	</div>
     );
